1eb73334cf541bc7d82b190e7ecc2fb56e125928
Emerson <emersonlfr1999@gmail.com>
2024-03-11 22:59:29 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center"> Chat Online </h1>
+
+<p align="center">
+  <img alt="projeto Chat Online" src=".github/preview.PNG" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Node
---
+++ servidor.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+const servidorHttp = http.createServer(app);
+const io = require("socket.io")(servidorHttp);
+
+io.addListener("connection", (socket) => {
+    console.log("um usuário conectou");
+    socket.addListener("nova mensagem", (msg) => {
+        io.emit("nova mensagem", msg);
+    })
+})
+
+app.use(express.static("public"));
+
+
+servidorHttp.listen(3000);
